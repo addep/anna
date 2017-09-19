ffdd4c6f16e3d70d0cd98dd4e3ae2f1a2bd20f4d
addep <jens.th@gmail.com>
2017-09-19 17:21:04 +0200

    emojilist with hero icons

--- anna.js
+++ anna.js
@@ -1,13 +1,12 @@
 const Discord = require("discord.js");
 const client = new Discord.Client();
 const fs = require("fs");
+const heroicon = require("./emoji.json");
 
 const config = require("./config.json");
 
-
-
 client.on("ready", () => {
-  console.log("I am ready");
+  console.log("Ready");
 });
 
 client.on("message", (message) => {
@@ -39,6 +38,12 @@ client.on("message", (message) => {
     }})
   }
 
+});
+client.on("message", (message) => {
+  if (message.content === "emojis") {
+    const emojiList = message.guild.emojis.map(e=>e.toString()).join(" ");
+    message.channel.send(emojiList);
+  }
 });
 
 client.login(config.token);
