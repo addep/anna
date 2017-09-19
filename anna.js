a468c153721842c936e893f4a8c3f977e9968334
addep <jens.th@gmail.com>
2017-09-19 12:53:29 +0200

    Installation, discord.js, anna.js and cofig

---
+++ anna.js
@@ -0,0 +1,15 @@
+const Discord = require(discord.js)
+const client = new Discord.Client();
+const config = require("\config.json");
+
+client.on("ready", () => {
+  console.log("I am ready");
+});
+
+client.on("message", (message) =>
+  if (message.content.startsWith("ping")) {
+    message.channel.send("pong");
+  }
+)};
+
+client.login(config.token);
