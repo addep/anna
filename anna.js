c8b69bcde2454847eaf7bd8ac8c5a4ec71d51c16
addep <jens.th@gmail.com>
2017-09-19 13:46:33 +0200

    removed npm install folder

--- anna.js
+++ anna.js
@@ -1,15 +1,35 @@
-const Discord = require(discord.js)
+const Discord = require("discord.js");
 const client = new Discord.Client();
 const config = require("./config.json");
+const fs = require("fs");
+
 
 client.on("ready", () => {
   console.log("I am ready");
 });
 
 client.on("message", (message) => {
-  if (message.content.startsWith("ping")) {
+  if (!message.content.startsWith(config.prefix) || message.author.bot) return;
+
+  if (message.content.startsWith(config.prefix + "ping")) {
     message.channel.send("pong!");
+  } else
+  if (message.content.startsWith(config.prefix + "foo")) {
+    message.channel.send("bar!");
   }
-)};
+});
+
+client.on("message", (message) => {
+  if(message.content.startsWith(config.prefix + "prefix")) {
+    // Gets the prefix from the command (eg. "!prefix +" it will take the "+" from it)
+    let newPrefix = message.content.split(" ").slice(1, 2)[0];
+    // change the configuration in memory
+    config.prefix = newPrefix;
+
+    // Now we have to save the file.
+    fs.writeFile("./config.json", JSON.stringify(config), (err) => console.error);
+    message.channel.send("prefix saved");
+  };
+});
 
 client.login(config.token);
