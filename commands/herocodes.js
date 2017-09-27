c9fa046ea3863d8df500298a15009f40a5f9d427
addep <jens.th@gmail.com>
2017-09-27 19:16:00 +0200

    command for retrieving herocodes to use with !embed command

---
+++ commands/herocodes.js
@@ -0,0 +1,25 @@
+const Discord = module.require("discord.js");
+const heroes = require("../heroes.json");
+
+module.exports.run = async (bot, message, args) => {
+
+  let title = "Heroes";
+  let lineup = "";
+  let msg = "";
+  let codeBlock = "```";
+  let herocount = Object.keys(heroes).length;
+  let heroArray = Object.keys(heroes);
+
+  for (var i = 0; i < herocount; i++){
+      let hero = heroes[heroArray[i]];
+      let emoji = hero.emoji;
+      msg += `${hero.keyword}` + `,\t`;
+  }
+
+  message.channel.send(codeBlock + msg + codeBlock);
+
+}
+
+module.exports.help = {
+  name: "herocodes"
+}
